3e38347ae68cebb6f24ac117fc33e5bde5781630
xiayesuifeng <1377195627@qq.com>
2018-03-06 13:27:18 +0800

    完成bootctl引导安装

--- core/bootloader/bootctl.go
+++ core/bootloader/bootctl.go
@@ -24,18 +24,8 @@ func (b *Bootctl) Install() error {
 	return nil
 }
 
-func (b *Bootctl) CreateEntries() error {
-	temp := `title   FireRain Linux
-linux   /vmlinuz-linux
-initrd  /initramfs-linux.img
-options root=PARTUUID={{ . }} rw`
-
-	t,err:=template.New("entries").Parse(temp)
-	if err != nil {
-		return err
-	}
-
-	file,err:=os.OpenFile(b.bootPath+	"/loader/entries/firerain.conf",os.O_CREATE | os.O_RDWR | os.O_TRUNC,755)
+func (b *Bootctl) Deploy() error {
+	err := b.Install()
 	if err != nil {
 		return err
 	}
@@ -45,7 +35,47 @@ options root=PARTUUID={{ . }} rw`
 		return err
 	}
 
-	err = t.Execute(file,partuuid)
+	b.CreateEntries("firerain.conf",partuuid,false)
+	b.CreateEntries("firerain-fallback.conf",partuuid,true)
+
+	b.SetDefaultEntries("firerain")
+
+	return nil
+}
+
+func (b *Bootctl) SetDefaultEntries(entriesName string) error {
+	file,err:=os.OpenFile(b.bootPath+"/loader/loader.conf",os.O_CREATE | os.O_RDWR | os.O_TRUNC,755)
+	if err != nil {
+		return err
+	}
+
+	_,err =file.Write([]byte("timeout 3\ndefault "+entriesName))
+	if err != nil {
+		return err
+	}
+
+	file.Close()
+
+	return nil
+}
+
+func (b *Bootctl) CreateEntries(confName,partuuid string,fallback bool) error {
+	temp := `title   FireRain Linux{{ if .fallback }}(fallback){{ endif }}
+linux   /vmlinuz-linux
+initrd  /initramfs-linux{{ if .fallback }}-fallback{{ endif }}.img
+options root=PARTUUID={{ .partuuid }} rw`
+
+	t,err:=template.New("entries").Parse(temp)
+	if err != nil {
+		return err
+	}
+
+	file,err:=os.OpenFile(b.bootPath+"/loader/entries/"+confName,os.O_CREATE | os.O_RDWR | os.O_TRUNC,755)
+	if err != nil {
+		return err
+	}
+
+	err = t.Execute(file,template.FuncMap{"partuuid":partuuid,"fallback":false})
 	file.Close()
 	return err
 }
