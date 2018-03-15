7b6bd67e3adc9800830e9a65b7f19551faef2836
xiayesuifeng <1377195627@qq.com>
2018-03-15 17:44:50 +0800

    完成patitionPage基本布局

--- ui/page/partitionPage.go
+++ ui/page/partitionPage.go
@@ -4,17 +4,23 @@ import (
 	"github.com/therecipe/qt/widgets"
 	"github.com/therecipe/qt/core"
 	widgets2 "github.com/firerainos/firerain-installer/ui/widgets"
+	"github.com/therecipe/qt/gui"
 )
 
 type PartitionPage struct {
 	*widgets.QFrame
+
+	tipLabel *widgets.QLabel
+
+	partitionList *widgets2.PartitionList
 }
 
 func NewPartitionPage(parent widgets.QWidget_ITF,fo core.Qt__WindowType) *PartitionPage {
 	frame := widgets.NewQFrame(parent,fo)
 
-	page := &PartitionPage{frame}
+	page := &PartitionPage{QFrame:frame}
 	page.init()
+	page.initConnect()
 
 	return page
 }
@@ -22,11 +28,24 @@ func NewPartitionPage(parent widgets.QWidget_ITF,fo core.Qt__WindowType) *Partit
 func (p *PartitionPage) init() {
 	vboxLayout := widgets.NewQVBoxLayout2(p)
 
-	welcomeLabel := widgets.NewQLabel2("Partition",p,0)
-	partitionList := widgets2.NewPartitionList(p)
+	logoLabel := widgets.NewQLabel(p,0)
+	welcomeLabel := widgets.NewQLabel2("FireRainOS",p,0)
+	p.tipLabel = widgets.NewQLabel2("请选择安装磁盘",p,0)
+	p.partitionList = widgets2.NewPartitionList(p)
 
+	logoLabel.SetPixmap(gui.NewQPixmap5("/home/linux/go/src/github.com/firerainos/firerain-installer/resources/logo.png","",0).Scaled2(200,200,core.Qt__KeepAspectRatioByExpanding,0))
+	logoLabel.SetFixedSize2(200,200)
+
+	vboxLayout.AddWidget(logoLabel,0,core.Qt__AlignCenter)
 	vboxLayout.AddWidget(welcomeLabel,0,core.Qt__AlignCenter)
-	vboxLayout.AddWidget(partitionList,0,core.Qt__AlignCenter)
+	vboxLayout.AddWidget(p.tipLabel,0,core.Qt__AlignCenter)
+	vboxLayout.AddWidget(p.partitionList,0,core.Qt__AlignCenter)
 
 	p.SetLayout(vboxLayout)
+}
+
+func (p *PartitionPage) initConnect() {
+	p.partitionList.ConnectPartitionItemChange(func() {
+		p.tipLabel.SetText("FireRainOS 将安装在磁盘\""+p.partitionList.CurrendItem().DevPath()+"\"上")
+	})
 }
