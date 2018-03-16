3a0e3ce41d350343013dbab548c50a191fd46223
xiayesuifeng <1377195627@qq.com>
2018-03-16 20:30:43 +0800

    完成wifi连接功能

--- ui/page/networkPage.go
+++ ui/page/networkPage.go
@@ -1,18 +1,32 @@
 package page
 
 import (
-	"github.com/therecipe/qt/widgets"
-	"github.com/therecipe/qt/core"
+	"github.com/firerainos/firerain-installer/core/networkmanager"
 	widgets2 "github.com/firerainos/firerain-installer/ui/widgets"
+	"github.com/therecipe/qt/core"
+	"github.com/therecipe/qt/widgets"
 )
 
 type NetworkPage struct {
 	*widgets.QFrame
+
+	wifiList *widgets.QListWidget
+
+	nm *networkmanager.NetworkManager
+
+	stopScan bool
 }
 
-func NewNetworkPage(parent widgets.QWidget_ITF,fo core.Qt__WindowType) *NetworkPage {
-	page := &NetworkPage{widgets.NewQFrame(parent,fo)}
+func NewNetworkPage(parent widgets.QWidget_ITF, fo core.Qt__WindowType) *NetworkPage {
+	page := &NetworkPage{QFrame: widgets.NewQFrame(parent, fo), nm: networkmanager.NewNetworkManager()}
 	page.init()
+
+	page.StartTimer(2000, core.Qt__PreciseTimer)
+	page.ConnectTimerEvent(func(event *core.QTimerEvent) {
+		if !page.stopScan {
+			page.scanWifi()
+		}
+	})
 
 	return page
 }
@@ -20,12 +34,49 @@ func NewNetworkPage(parent widgets.QWidget_ITF,fo core.Qt__WindowType) *NetworkP
 func (n *NetworkPage) init() {
 	vboxLayout := widgets.NewQVBoxLayout2(n)
 
-	networkLabel := widgets.NewQLabel2("Network",n,0)
+	networkLabel := widgets.NewQLabel2("Network", n, 0)
+	n.wifiList = widgets.NewQListWidget(n)
 
-	wifiList := widgets2.NewWifiList(n)
+	n.wifiList.SetMinimumSize2(480, 500)
 
-	vboxLayout.AddWidget(networkLabel,0,core.Qt__AlignCenter)
-	vboxLayout.AddWidget(wifiList,0,core.Qt__AlignCenter)
+	vboxLayout.AddWidget(networkLabel, 0, core.Qt__AlignCenter)
+	vboxLayout.AddWidget(n.wifiList, 0, core.Qt__AlignCenter)
 
 	n.SetLayout(vboxLayout)
+}
+
+func (n *NetworkPage) scanWifi() {
+	n.nm.WifiScan()
+
+	wifiList := *n.nm.WifiList()
+
+	n.wifiList.Clear()
+
+	for _, wifiInfo := range wifiList {
+		item := widgets.NewQListWidgetItem(n.wifiList, 0)
+		item.SetSizeHint(core.NewQSize2(460, 50))
+		listItem := widgets2.NewWifiListItem(n, 0)
+		listItem.SetWifiInfo(wifiInfo)
+		n.wifiList.SetItemWidget(item, listItem)
+
+		listItem.ConnectWifiListItemClicked(n.onWifiListItemClicked)
+	}
+}
+
+func (n *NetworkPage) onWifiListItemClicked(ssid string, security string, inUse bool) {
+	n.stopScan = true
+	if security == "" {
+		n.nm.ConnectWifi(ssid, "")
+	} else {
+		dialog := widgets.NewQInputDialog(n, 0)
+		password := dialog.GetText(n, "提示", "请输入"+ssid+"的密码", widgets.QLineEdit__Password, "", true, 0, 0)
+		if password == "" {
+			n.scanWifi()
+			n.stopScan = false
+			return
+		}
+		n.nm.ConnectWifi(ssid, password)
+	}
+	n.stopScan = false
+	n.scanWifi()
 }
--- ui/widgets/wifiListItem.go
+++ ui/widgets/wifiListItem.go
@@ -1,51 +1,75 @@
 package widgets
 
 import (
-	"github.com/therecipe/qt/widgets"
-	"github.com/therecipe/qt/core"
 	"github.com/firerainos/firerain-installer/core/networkmanager"
+	"github.com/therecipe/qt/core"
+	"github.com/therecipe/qt/widgets"
 	"strconv"
 )
 
 type WifiListItem struct {
-	*widgets.QWidget
+	widgets.QWidget
 
 	wifInfo networkmanager.WifiInfo
 
 	btn *widgets.QPushButton
+
+	signalLabel, ssidLabel, securityLabel *widgets.QLabel
+
+	_ func() `constructor:"init()"`
+
+	_ func(ssid, security string, inUse bool) `signal:"wifiListItemClicked"`
 }
 
-func NewWifiListItem(wifiInfo networkmanager.WifiInfo,parent widgets.QWidget_ITF, fo core.Qt__WindowType) *WifiListItem {
-	widget := widgets.NewQWidget(parent, fo)
-
-	wifiListItem := &WifiListItem{QWidget: widget,wifInfo:wifiInfo}
-	wifiListItem.init()
-
-	return wifiListItem
-}
+//func NewWifiListItem2(wifiInfo networkmanager.WifiInfo,parent widgets.QWidget_ITF, fo core.Qt__WindowType) *WifiListItem {
+//	widget := widgets.NewQWidget(parent, fo)
+//
+//	wifiListItem := &WifiListItem{QWidget: *widget,wifInfo:wifiInfo}
+//	wifiListItem.init()
+//
+//	return wifiListItem
+//}
 
 func (w *WifiListItem) init() {
-	w.SetMinimumSize2(460,50)
+	w.SetMinimumSize2(460, 50)
 
 	hboxLayout := widgets.NewQHBoxLayout()
-	hboxLayout.SetContentsMargins(10,10,10,10)
+	hboxLayout.SetContentsMargins(10, 10, 10, 10)
 	hboxLayout.SetSpacing(50)
 
-	signalLabel := widgets.NewQLabel2(strconv.Itoa(w.wifInfo.Signal),w,0)
-	ssidLabel := widgets.NewQLabel2(w.wifInfo.Ssid,w,0)
-	securityLabel := widgets.NewQLabel2(w.wifInfo.Security,w,0)
+	w.signalLabel = widgets.NewQLabel(w, 0)
+	w.ssidLabel = widgets.NewQLabel(w, 0)
+	w.securityLabel = widgets.NewQLabel(w, 0)
 
-	if w.wifInfo.InUse {
-		w.btn = widgets.NewQPushButton2("断开",w)
-	} else {
-		w.btn = widgets.NewQPushButton2("连接",w)
-	}
+	w.btn = widgets.NewQPushButton(w)
 
-	hboxLayout.AddWidget(signalLabel,0,core.Qt__AlignLeft)
-	hboxLayout.AddWidget(ssidLabel,0,core.Qt__AlignLeft)
+	hboxLayout.AddWidget(w.signalLabel, 0, core.Qt__AlignLeft)
+	hboxLayout.AddWidget(w.ssidLabel, 0, core.Qt__AlignLeft)
 	hboxLayout.AddStretch(10)
-	hboxLayout.AddWidget(securityLabel,0,core.Qt__AlignRight)
-	hboxLayout.AddWidget(w.btn,0,core.Qt__AlignRight)
+	hboxLayout.AddWidget(w.securityLabel, 0, core.Qt__AlignRight)
+	hboxLayout.AddWidget(w.btn, 0, core.Qt__AlignRight)
 
 	w.SetLayout(hboxLayout)
+
+	w.btn.ConnectClicked(func(checked bool) {
+		w.btn.SetText("连接中")
+		w.btn.SetEnabled(false)
+		w.WifiListItemClicked(w.wifInfo.Ssid, w.wifInfo.Security, w.wifInfo.InUse)
+	})
+}
+
+func (w *WifiListItem) SetWifiInfo(wifiInfo networkmanager.WifiInfo) {
+	w.wifInfo = wifiInfo
+
+	w.signalLabel.SetText(strconv.Itoa(w.wifInfo.Signal))
+	w.ssidLabel.SetText(w.wifInfo.Ssid)
+	w.securityLabel.SetText(w.wifInfo.Security)
+
+	if w.wifInfo.InUse {
+		w.btn.SetText("已连接")
+		w.btn.SetEnabled(false)
+	} else {
+		w.btn.SetText("连接")
+		w.btn.SetEnabled(true)
+	}
 }
