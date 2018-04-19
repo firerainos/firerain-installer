e1b42fec13ed0ad17e6e216193ff0839aa06d8d4
xiayesuifeng <1377195627@qq.com>
2018-04-19 23:55:38 +0800

    add account

---
+++ config/account.go
@@ -0,0 +1,14 @@
+package config
+
+type Account struct {
+	Username string
+	Password string
+}
+
+func (a *Account) SetUsername(username string) {
+	a.Username=username
+}
+
+func (a *Account) SetPassword(password string) {
+	a.Password=password
+}
---
+++ styles/nextButton.go
@@ -0,0 +1,3 @@
+package styles
+
+const NextButton string = ``
