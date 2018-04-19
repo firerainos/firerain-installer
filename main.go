7a6dcd406b622e1af5a5cb1d24a8f58deb91bbd7
xiayesuifeng <1377195627@qq.com>
2018-04-20 00:08:10 +0800

    init config package

--- main.go
+++ main.go
@@ -4,9 +4,12 @@ import (
 	"github.com/firerainos/firerain-installer/ui"
 	"github.com/therecipe/qt/widgets"
 	"os"
+	"github.com/firerainos/firerain-installer/config"
 )
 
 func main() {
+	config.InitConfig()
+
 	widgets.NewQApplication(len(os.Args), os.Args)
 
 	ui.NewMainWindow().Show()
