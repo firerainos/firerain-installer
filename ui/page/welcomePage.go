7ff744064058d7fe67b14cf59844f4d2b97fa49c
xiayesuifeng <1377195627@qq.com>
2018-04-19 21:18:01 +0800

    use helloWidget

--- ui/page/welcomePage.go
+++ ui/page/welcomePage.go
@@ -2,8 +2,8 @@ package page
 
 import (
 	"github.com/therecipe/qt/core"
-	"github.com/therecipe/qt/multimedia"
 	"github.com/therecipe/qt/widgets"
+	widgets2 "github.com/firerainos/firerain-installer/ui/widgets"
 )
 
 type WelcomePage struct {
@@ -22,20 +22,9 @@ func (w *WelcomePage) init() {
 	vboxLayout := widgets.NewQVBoxLayout2(w)
 	vboxLayout.SetContentsMargins(0, 0, 0, 0)
 
-	mediaPlayer := multimedia.NewQMediaPlayer(w, 0)
-	videoWidget := multimedia.NewQVideoWidget(w)
-	playerlist := multimedia.NewQMediaPlaylist(w)
-	playerlist.AddMedia(multimedia.NewQMediaContent2(core.QUrl_FromLocalFile("/home/linux/go/src/github.com/firerainos/firerain-installer/resources/hello.mp4")))
-	//playerlist.AddMedia(multimedia.NewQMediaContent2(core.QUrl_FromLocalFile("resources/hello.mp4")))
-	playerlist.SetCurrentIndex(1)
-	playerlist.SetPlaybackMode(multimedia.QMediaPlaylist__CurrentItemInLoop)
-	mediaPlayer.SetPlaylist(playerlist)
-	mediaPlayer.SetVideoOutput(videoWidget)
-	mediaPlayer.Play()
+	hellWidget := widgets2.NewHelloWidget(w,0)
 
-	videoWidget.SetFixedSize2(900, 505)
-
-	vboxLayout.AddWidget(videoWidget, 0, core.Qt__AlignCenter)
+	vboxLayout.AddWidget(hellWidget, 0, core.Qt__AlignCenter)
 	vboxLayout.AddStretch(1)
 
 	w.SetLayout(vboxLayout)
