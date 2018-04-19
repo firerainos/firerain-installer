4cdf2ed0b776dfa97d7d00702e5d6cf9f196e06e
xiayesuifeng <1377195627@qq.com>
2018-04-19 16:31:55 +0800

    add LineEdit widget

---
+++ styles/lineEdit.go
@@ -0,0 +1,15 @@
+package styles
+
+const LineEdit string = `QLineEdit {
+            border: 1px solid transparent;
+            border-radius: 4px;
+            padding-left: 32px;
+            outline: none;
+            font-size: 12px;
+            line-height: 1.25;
+          }
+          
+          QLineEdit:focus {
+            border: 1px solid #2ca7f8;
+          }
+`
---
+++ ui/widgets/lineEdit.go
@@ -0,0 +1,37 @@
+package widgets
+
+import (
+	"github.com/therecipe/qt/widgets"
+	"github.com/firerainos/firerain-installer/styles"
+	"github.com/therecipe/qt/core"
+	"github.com/therecipe/qt/gui"
+)
+
+type LineEdit struct {
+	*widgets.QLineEdit
+
+	iconLabel *widgets.QLabel
+}
+
+func NewLineEdit(icon string,parent widgets.QWidget_ITF) *LineEdit {
+	widget := widgets.NewQLineEdit(parent)
+
+	lineEdit := &LineEdit{QLineEdit: widget}
+	lineEdit.init(icon)
+
+	return lineEdit
+}
+
+func (lineEdit *LineEdit) init(icon string) {
+	lineEdit.iconLabel = widgets.NewQLabel(lineEdit,0)
+
+	lineEdit.iconLabel.SetPixmap(gui.NewQPixmap5(icon,"",0))
+	lineEdit.iconLabel.SetFixedSize2(12,12)
+	lineEdit.iconLabel.Move2(12,12)
+
+	lineEdit.SetStyleSheet(styles.LineEdit)
+
+	lineEdit.SetFixedSize2(310, 36)
+
+	lineEdit.SetContextMenuPolicy(core.Qt__NoContextMenu)
+}
