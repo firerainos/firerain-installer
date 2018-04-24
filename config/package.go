4b5080fb68362af5636ff141be7f17d56678aaa3
xiayesuifeng <1377195627@qq.com>
2018-04-24 18:51:27 +0800

    add search function for package

--- config/package.go
+++ config/package.go
@@ -17,3 +17,12 @@ func (p *Package) RemovePackage(pkg string) {
 	}
 	p.PkgList=list
 }
+
+func (p *Package) SearchPackage(pkg string) bool {
+	for _, name := range p.PkgList {
+		if name == pkg {
+			return true
+		}
+	}
+	return false
+}
