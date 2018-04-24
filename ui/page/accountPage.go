a9fa1142ffb99c4c30dd2613d0332cb58ed19f70
xiayesuifeng <1377195627@qq.com>
2018-04-24 23:56:04 +0800

    add tipLabel for accountPage

--- ui/page/accountPage.go
+++ ui/page/accountPage.go
@@ -10,6 +10,10 @@ import (
 
 type AccountPage struct {
 	*widgets.QFrame
+
+	tipsLabel *widgets.QLabel
+
+	username,password *widgets2.LineEdit
 }
 
 func NewAccountPage(parent widgets.QWidget_ITF, fo core.Qt__WindowType) *AccountPage {
@@ -28,24 +32,38 @@ func (page *AccountPage) init() {
 
 	loginLabel := widgets.NewQLabel2("登陆", page, 0)
 
-	username := widgets2.NewLineEdit(pwd+"/resources/username.svg", page)
-	password := widgets2.NewLineEdit(pwd+"/resources/password.svg", page)
-	password.SetEchoMode(widgets.QLineEdit__Password)
+	page.tipsLabel = widgets.NewQLabel(page,0)
+	page.tipsLabel.SetAlignment(core.Qt__AlignCenter)
+
+	page.username = widgets2.NewLineEdit(pwd+"/resources/username.svg", page)
+	page.password = widgets2.NewLineEdit(pwd+"/resources/password.svg", page)
+	page.password.SetEchoMode(widgets.QLineEdit__Password)
 
 	vboxLayout.AddStretch(1)
 	vboxLayout.AddWidget(loginLabel, 0, core.Qt__AlignCenter)
+	vboxLayout.AddWidget(page.tipsLabel, 0, core.Qt__AlignCenter)
 	vboxLayout.AddSpacing(100)
-	vboxLayout.AddWidget(username, 0, core.Qt__AlignHCenter)
-	vboxLayout.AddWidget(password, 0, core.Qt__AlignHCenter)
+	vboxLayout.AddWidget(page.username, 0, core.Qt__AlignHCenter)
+	vboxLayout.AddWidget(page.password, 0, core.Qt__AlignHCenter)
 	vboxLayout.AddStretch(1)
 
 	page.SetLayout(vboxLayout)
 
-	username.ConnectLeaveEvent(func(event *core.QEvent) {
-		config.Conf.SetUsername(username.Text())
+	page.username.ConnectLeaveEvent(func(event *core.QEvent) {
+		config.Conf.SetUsername(page.username.Text())
 	})
 
-	password.ConnectLeaveEvent(func(event *core.QEvent) {
-		config.Conf.SetPassword(password.Text())
+	page.password.ConnectLeaveEvent(func(event *core.QEvent) {
+		config.Conf.SetPassword(page.password.Text())
 	})
 }
+
+func  (page *AccountPage) SetTips(tips string) {
+	page.tipsLabel.SetText(tips)
+}
+
+func  (page *AccountPage) SetEnableLogin(enable bool) {
+	page.username.SetEnabled(enable)
+	page.password.SetEnabled(enable)
+}
+
