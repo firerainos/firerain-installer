e00f3f0da16d3121675b2eb8f71e19fdb206e0b2
xiayesuifeng <1377195627@qq.com>
2018-04-25 23:53:03 +0800

    done installPage base layout

--- ui/page/installPage.go
+++ ui/page/installPage.go
@@ -7,12 +7,16 @@ import (
 
 type InstallPage struct {
 	*widgets.QFrame
+
+	tipsLabel *widgets.QLabel
+
+	messageText *widgets.QTextBrowser
 }
 
 func NewInstallPage(parent widgets.QWidget_ITF, fo core.Qt__WindowType) *InstallPage {
 	frame := widgets.NewQFrame(parent, fo)
 
-	page := &InstallPage{frame}
+	page := &InstallPage{QFrame:frame}
 	page.init()
 
 	return page
@@ -21,9 +25,22 @@ func NewInstallPage(parent widgets.QWidget_ITF, fo core.Qt__WindowType) *Install
 func (i *InstallPage) init() {
 	vboxLayout := widgets.NewQVBoxLayout2(i)
 
-	welcomeLabel := widgets.NewQLabel2("Install", i, 0)
+	welcomeLabel := widgets.NewQLabel2("FireRainOS安装", i, 0)
 
+	i.tipsLabel = widgets.NewQLabel(i,0)
+	i.tipsLabel.SetFixedWidth(200)
+	i.tipsLabel.SetAlignment(core.Qt__AlignCenter)
+
+	i.messageText = widgets.NewQTextBrowser(i)
+	i.messageText.SetFixedSize2(800,500)
+
+	vboxLayout.AddSpacing(80)
 	vboxLayout.AddWidget(welcomeLabel, 0, core.Qt__AlignCenter)
+	vboxLayout.AddStretch(1)
+	vboxLayout.AddWidget(i.tipsLabel, 0, core.Qt__AlignHCenter)
+	vboxLayout.AddStretch(1)
+	vboxLayout.AddWidget(i.messageText,0,core.Qt__AlignCenter)
+	vboxLayout.AddSpacing(50)
 
 	i.SetLayout(vboxLayout)
 }
