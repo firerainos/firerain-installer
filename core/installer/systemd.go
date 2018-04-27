d8c306ea8eacc6767f80a4c0deb04e431cd53015
xiayesuifeng <1377195627@qq.com>
2018-04-28 00:00:33 +0800

    add systemd service enable function

---
+++ core/installer/systemd.go
@@ -0,0 +1,44 @@
+package installer
+
+import (
+	"github.com/firerainos/firerain-installer/config"
+	"os/exec"
+)
+
+func EnableServices() error {
+	if err := Chroot(); err != nil {
+		return err
+	}
+
+	for _, pkg := range config.Conf.PkgList {
+		var err error
+		switch pkg {
+		case "sddm":
+			err = EnableService("sddm")
+		case "lightdm":
+			err = EnableService("lightdm")
+		case "networkmanager":
+			err = EnableService("NetworkManager")
+		case "teamviewer":
+			fallthrough
+		case "teamviewer-beta":
+			err = EnableService("teamviewerd")
+		case "bumblebee":
+			err = EnableService("bumblebeed")
+		case "tlp":
+			if err = EnableService("tlp");err!=nil {
+				break
+			}
+			err = EnableService("tlp-sleep")
+		}
+		if err != nil {
+			return err
+		}
+	}
+}
+
+func EnableService(service string) error {
+	cmd := exec.Command("systemctl", "enable", service)
+	return cmd.Run()
+
+}
