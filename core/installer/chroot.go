61a10ad8cffb6bead995199c67e489587ab0ac2f
xiayesuifeng <1377195627@qq.com>
2018-04-27 23:43:13 +0800

    add chroot.go

---
+++ core/installer/chroot.go
@@ -0,0 +1,27 @@
+package installer
+
+import (
+	"os"
+	"syscall"
+)
+
+var root *os.File
+
+func Chroot() error {
+	var err error
+	root,err=os.Open("/")
+	if err != nil {
+		return err
+	}
+
+	return syscall.Chroot("/mnt")
+}
+
+func ExitChroot() error {
+	if err:=root.Chdir();err!=nil{
+		return err
+	}
+	defer root.Close()
+
+	return syscall.Chroot(".")
+}
