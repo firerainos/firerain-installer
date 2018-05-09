1d00ba17512545c50089d612e970ce7752e40d98
xiayesuifeng <1377195627@qq.com>
2018-05-10 00:02:26 +0800

    add IsUEFI field

--- config/config.go
+++ config/config.go
@@ -1,21 +1,29 @@
 package config
 
+import "os"
+
 var Conf *Config
 
 type Config struct {
 	*Account
 	*Package
 	InstallDev string
+	IsUEFI bool
 	EFIDev string
 }
 
 func InitConfig() {
-	Conf = &Config{Account: &Account{}, Package: &Package{}}
+	Conf = &Config{Account: &Account{}, Package: &Package{},IsUEFI:true}
 	Conf.AddPackage("base")
 	Conf.AddPackage("base-devel")
 	Conf.AddPackage("vim")
 	Conf.AddPackage("sudo")
 	Conf.AddPackage("networkmanager")
+	if _,err:=os.Stat("/sys/firmware/efi/efivars");err!=nil{
+		if os.IsNotExist(err) {
+			Conf.IsUEFI = false
+		}
+	}
 }
 
 func (config *Config) SetInstallDev(dev string) {
