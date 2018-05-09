83e0a26f8da3f4ce82c03077c8da03be19280124
xiayesuifeng <1377195627@qq.com>
2018-05-10 00:03:38 +0800

    done mount.go

--- core/installer/mount.go
+++ core/installer/mount.go
@@ -3,17 +3,33 @@ package installer
 import (
 	"github.com/firerainos/firerain-installer/config"
 	"syscall"
+	"os"
 )
 
 func MountMnt() error {
-	return syscall.Mount(config.Conf.InstallDev, "/mnt", "", syscall.MS_BIND, "")
+	err := syscall.Mount(config.Conf.InstallDev, "/mnt", "", 0, "")
+	if err != nil {
+		return err
+	}
+	err = os.Mkdir("/mnt/boot",os.FileMode(0755))
+	if err != nil {
+		return err
+	}
+	return syscall.Mount(config.Conf.EFIDev,"/mnt/boot","",0,"")
+}
+
+func UnMountMnt() {
+	syscall.Unmount("/mnt/boot",0)
+	syscall.Unmount("/mnt",0)
 }
 
 func BindMnt() {
 	syscall.Mount("proc", "/mnt/proc", "proc", syscall.MS_NOSUID|syscall.MS_NOEXEC|syscall.MS_NODEV, "")
 	syscall.Mount("sys", "/mnt/sys", "sysfs", syscall.MS_NOSUID|syscall.MS_NOEXEC|syscall.MS_NODEV, "ro")
-	syscall.Mount("efivarfs", "/mnt/sys/firmware/efi/efivars", "efivarfs",
-		syscall.MS_NOSUID|syscall.MS_NOEXEC|syscall.MS_NODEV, "")
+	if config.Conf.IsUEFI {
+		syscall.Mount("efivarfs", "/mnt/sys/firmware/efi/efivars", "efivarfs",
+			syscall.MS_NOSUID|syscall.MS_NOEXEC|syscall.MS_NODEV, "")
+	}
 	syscall.Mount("udev","/mnt/dev","devtmpfs",syscall.MS_NOSUID,"mode=0755")
 	syscall.Mount("devpts","/mnt/dev/pts","devpts",
 		syscall.MS_NOSUID | syscall.MS_NOEXEC,"mode=0620,gid=5")
