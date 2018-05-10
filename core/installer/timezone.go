89695a81199ba929ff1919805bf5e3fdde0792e2
xiayesuifeng <1377195627@qq.com>
2018-05-10 16:49:49 +0800

    add set timezone function

---
+++ core/installer/timezone.go
@@ -0,0 +1,43 @@
+package installer
+
+import (
+	"net/http"
+	"encoding/json"
+	"os"
+	"os/exec"
+)
+
+type IpApi struct {
+	Query string `json:"query"`
+	Timezone string `json:"timezone"`
+}
+
+func AutoSetTimezone() error {
+	timezone,err := getTimezone()
+	if err != nil {
+		return err
+	}
+
+	if err:=os.Symlink("/usr/share/zoneinfo/"+timezone,"/tmp/localtime");err!=nil{
+		return err
+	}
+
+	cmd:= exec.Command("hwclock","--systohc")
+	return cmd.Run()
+}
+
+func getTimezone() (string,error) {
+	resp,err:=http.Get("http://ip-api.com/json")
+	if err != nil {
+		return "",err
+	}
+	defer resp.Body.Close()
+
+	ipApi := IpApi{}
+
+	if err := json.NewDecoder(resp.Body).Decode(&ipApi);err!=nil{
+		return "",err
+	}
+
+	return ipApi.Timezone,nil
+}
