f556dc31961c4f03ec90254c56e82de92aec591a
xiayesuifeng <1377195627@qq.com>
2018-05-15 22:42:36 +0800

    rescan when partition changes

--- ui/widgets/partitionList.go
+++ ui/widgets/partitionList.go
@@ -1,12 +1,15 @@
 package widgets
 
 import (
+	"github.com/firerainos/firerain-installer/config"
 	"github.com/firerainos/firerain-installer/core/parted"
+	"github.com/firerainos/firerain-installer/styles"
+	"github.com/fsnotify/fsnotify"
 	"github.com/therecipe/qt/core"
 	"github.com/therecipe/qt/widgets"
-	"github.com/firerainos/firerain-installer/config"
+	"os/exec"
 	"strconv"
-	"github.com/firerainos/firerain-installer/styles"
+	"strings"
 )
 
 type PartitionList struct {
@@ -16,13 +19,19 @@ type PartitionList struct {
 	buttonGroup *widgets.QButtonGroup
 	listItems   []*PartitionListItem
 
+	openButton *widgets.QPushButton
+
 	parted *parted.Parted
+
+	watcher *fsnotify.Watcher
+	devList []string
 
 	currendItem *PartitionListItem
 
 	_ func() `constructor:"init"`
 
 	_ func() `signal:"partitionItemChange"`
+	_ func() `signal:"deviceChange"`
 }
 
 func (p *PartitionList) init() {
@@ -38,11 +47,39 @@ func (p *PartitionList) init() {
 
 	p.buttonGroup = widgets.NewQButtonGroup(p)
 
+	p.openButton = widgets.NewQPushButton2("打开分区管理器", p)
+	p.openButton.SetVisible(false)
+	p.openButton.SetGeometry2(185, 120, 130, 50)
+
 	frame.SetLayout(p.hboxLayout)
 
 	p.SetWidget(frame)
 
 	p.parted = parted.NewParted()
+
+	p.watcher, _ = fsnotify.NewWatcher()
+	p.watcher.Add("/dev")
+
+	go func() {
+		for {
+			event := <-p.watcher.Events
+			name := ""
+			if event.Op&fsnotify.Create == fsnotify.Create {
+				name = event.Name
+			} else if event.Op&fsnotify.Remove == fsnotify.Remove {
+				name = event.Name
+			}
+
+			if name != "" {
+				for _, dev := range p.devList {
+					if strings.HasPrefix(name, dev) {
+						p.DeviceChange()
+						break
+					}
+				}
+			}
+		}
+	}()
 
 	p.initConnect()
 	p.ScanPartition()
@@ -52,6 +89,14 @@ func (p *PartitionList) initConnect() {
 	p.buttonGroup.ConnectButtonClicked2(func(id int) {
 		p.currendItem = p.listItems[id]
 		p.PartitionItemChange()
+	})
+
+	p.openButton.ConnectClicked(func(checked bool) {
+		exec.Command("/usr/bin/partitionmanager").Start()
+	})
+
+	p.ConnectDeviceChange(func() {
+		p.ScanPartition()
 	})
 }
 
@@ -80,7 +125,9 @@ func (p *PartitionList) ScanPartition() {
 		return
 	}
 
+	p.devList = make([]string, 0)
 	for _, dev := range devices {
+		p.devList = append(p.devList, dev.Disk)
 		for _, partition := range dev.Partitions {
 			if config.Conf.EFIDev == "" && (partition.FileSystem == "fat16" || partition.FileSystem == "fat32") {
 				config.Conf.SetEFIDev(partition.Device.Disk + strconv.Itoa(partition.Number))
@@ -97,6 +144,8 @@ func (p *PartitionList) ScanPartition() {
 			item.SetSelect(config.Conf.InstallDev == item.DevPath())
 		}
 	}
+
+	p.openButton.SetVisible(len(p.listItems) == 0)
 }
 
 func (p *PartitionList) CurrendItem() *PartitionListItem {
