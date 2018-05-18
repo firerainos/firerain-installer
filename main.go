cf731131942cb5bf5d340984a0da43ebb435d291
xiayesuifeng <1377195627@qq.com>
2018-05-19 01:08:36 +0800

    set window icon

--- main.go
+++ main.go
@@ -13,6 +13,7 @@ func main() {
 	app := widgets.NewQApplication(len(os.Args), os.Args)
 	app.SetApplicationVersion("0.1.0")
 	app.SetApplicationName("FireRain安装器")
+	app.SetWindowIcon(gui.NewQIcon5("/usr/share/icons/hicolor/192x192/apps/firerain-installer.svg"))
 
 	ui.NewMainWindow().Show()
 
